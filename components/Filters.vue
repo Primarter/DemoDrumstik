26bec1d7e3e95e09f79d2a4be11d0a3ae0538462
Primarter <alec.ferrari@epitech.eu>
2020-06-16 17:42:04 +0200

    feat(master): removed on-click size modifications on filter

--- components/Filters.vue
+++ components/Filters.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="filters-wrapper">
-    <div class="my-btn" @click="updateFilter(0, 'all')" :style="linkStyle[0] + 'display: inline-block; min-width: 33%'">Tous</div>
-    <div class="my-btn" @click="updateFilter(1, 'files')" :style="linkStyle[1] + 'display: inline-block; min-width: 33%'">Mes fichiers</div>
-    <div class="my-btn" @click="updateFilter(2, 'favorites')" :style="linkStyle[2] + 'display: inline-block; min-width: 33%'">Favoris</div>
+    <div class="my-btn filter" @click="updateFilter(0, 'all')" :style="linkStyle[0] + 'min-width: 33% !important; max-width: 33% !important; width: 33% !important'">Tous</div>
+    <div class="my-btn filter" @click="updateFilter(1, 'files')" :style="linkStyle[1] + 'min-width: 33% !important; max-width: 33% !important; width: 33% !important'">Mes fichiers</div>
+    <div class="my-btn filter" @click="updateFilter(2, 'favorites')" :style="linkStyle[2] + 'min-width: 33% !important; max-width: 33% !important; width: 33% !important'">Favoris</div>
   </div>
 </template>
 
@@ -40,10 +40,12 @@ export default {
   text-align: center;
   width: 100%;
   display: table;
-  table-layout: fixed;
   vertical-align: middle;
   color: #60dfe8;
 }
+/* 
+background-color: rgb(22, 24, 31); min-width: 33%; max-width: 33%;
+background-color:#16181f;display:inline-block;min-width:33%;max-width:33%; */
 
 .filter {
   display: block;
